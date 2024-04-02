88b8798d83ef54153dc5ed10265bbf4dd96a15e6
akshayhiranwal1 <akshayhiranwal1@gmail.com>
2024-04-02 02:21:37 +0100

    changes for removing unwanted references

--- src/main/java/com/SIS/RetrospectiveService/repository/FeedRepo.java
+++ src/main/java/com/SIS/RetrospectiveService/repository/FeedRepo.java
@@ -2,6 +2,8 @@ package com.SIS.RetrospectiveService.repository;
 
 import com.SIS.RetrospectiveService.model.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FeedRepo extends JpaRepository<Feedback, Integer> {
 }
